0b49454102531b01efe0aff6b0d7ead022cc1f27
刘清 <liuqing@Noe.local>
2015-06-18 09:27:34 +0800

    Initial Commit

---
+++ OCFile/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCFile
+//
+//  Created by 刘清 on 15/6/18.
+//  Copyright (c) 2015年 LiuQing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
